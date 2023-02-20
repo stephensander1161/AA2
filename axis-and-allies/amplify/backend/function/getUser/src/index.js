3d0c14158b35bda2924d420be46096148e6d3072
stephensander1161 <57675208+stephensander1161@users.noreply.github.com>
2023-02-19 18:31:24 -0700

    initial FE and BE setup

---
+++ axis-and-allies/amplify/backend/function/getUser/src/index.js
@@ -0,0 +1,16 @@
+/**
+ * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
+ */
+exports.handler = async (event) => {
+  console.log(`EVENT: ${JSON.stringify(event)}`);
+  const name = event.pathParameters.userId;
+  return {
+    statusCode: 200,
+    //  Uncomment below to enable CORS requests
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "*",
+    },
+    body: JSON.stringify(name + " SMELLS LIKE CHEESE!"),
+  };
+};
---
+++ axis-and-allies/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,18 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "apie2312971": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "getUser": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}

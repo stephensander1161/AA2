77e0421354be1f24dca44433d3e063d03470172b
stephensander1161 <57675208+stephensander1161@users.noreply.github.com>
2023-02-20 20:52:45 -0700

    clean out test db code

--- axis-and-allies/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ axis-and-allies/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,11 +1,4 @@
 export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "apie2312971": {
-      "ApiId": "string",
-      "ApiName": "string",
-      "RootUrl": "string"
-    }
-  },
   "function": {
     "getUser": {
       "Arn": "string",
